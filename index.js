13930a5ec742d158a16601c47fa525d4e16a464b
Sharon Miller <sharon@sharonmillercreative.com>
2018-09-14 10:49:50 -0700

    added dm and quit, code is functional

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //moduels: startserver, User with constructor, socketpool, events, parseBuffer
 //modules for commands  under actions, a module for each command
 //server listeners would go in app.js along with startserver, dispatch action, handle connection, server.on
-//use requireDir() node helper to require directires
+//use requireDir() node helper to require directories
 
 'use strict';
 const COMMAND_SYMBOL = '@';
@@ -21,7 +21,7 @@ let User = function (socket) {
   let id = uuid(); // QUESTION: I am getting confused with ID and how it is passed- sometimes user.id
   this.id = id;
   this.nickname = `User-${id}`;
-  this.socket = socket; 
+  this.socket = socket;
 };
 
 // console.log();
@@ -33,45 +33,63 @@ server.on('connection', (socket) => {
   console.log('above event listener');
   socket.on('data', (buffer) => {
     dispatchAction(user, buffer);
-  });//this would go in app.js
+  });
 });
 
 let dispatchAction = (user, buffer) => {
   let message = parse(buffer);
-  events.emit(message.command, user.id, message);  
-  console.log(message);
-}; //this would go in app.js
+  message && events.emit(message.command, user.id, message);
+};
 
+/* on events */
+/* code for on event listener for @all */
 events.on('@all', (senderId, message) => {
-  for (let userId in userPool) { //this is a different userId - The for/in statement loops through the properties of an object. The block of code inside the loop will be executed once for each property.
+  for (let userId in userPool) {
     let user = userPool[userId];
     user.socket.write(`<${senderId}>: ${message.payload}\n`);
-  } 
+  }
 });
-//code for on event listener for @list command 
-events.on('@list', (senderId) => { //why is message not uuid
+
+/* code for on event listener for @list command  */
+events.on('@list', (senderId) => {
   let user = userPool[senderId];
-  // console.log(sender);
-  console.log(user);
   for (let userId in userPool) {
     user.socket.write(`<${userPool[userId].nickname}>\n`);
   }
 });
 
-//change name to nickname with @nickname <new-name>
+/* code for on event listener for @nickname */
+//change name to nickname with @nickname <new-name> 
+/* //QUESTION: after I change the nickname, it carries through to the quit command, but not the all conmand(which still lists my userid string), why? */
 events.on('@nickname', (senderId, message) => {
   let user = userPool[senderId];
-  console.log('my nickname:', message.payload);
-  console.log(userPool[senderId]);
   user.nickname = message.payload;
-  // console.log(user.nickname);
   user.socket.write(`<Your new username is ${user.nickname}>.\n`);
 });
 
+/* code to allow user to quit  */
+events.on('@quit', (senderId) => {
+  let user = userPool[senderId];
+  user.socket.write(`Adios ${user.nickname}`);
+  user.socket.destroy();
+  user.socket.emit('close');
+});
 
+/* code to allow user to send direct message */
+events.on('@dm', (senderId, message, ) => {
+  let user = userPool[senderId].nickname;
+  let receiverId = message.payload.match(/[^\s]+/)[0];
 
+  for (let userId in userPool) {
+    if (userPool[userId].nickname === receiverId) {
+      let user = userPool[userId];
+      user.socket.write(`<${user}> ${message.payload.match(/[\s].*/gm)}\n`);
+    }
+  }
+});
+
+/* code to parse the messages */
 let parse = (buffer) => {
-  console.log('Im in parse');
   let text = buffer.toString().trim();
   if (!text.startsWith(COMMAND_SYMBOL)) { return null; }
   if (text.startsWith(`${COMMAND_SYMBOL}`)) {
@@ -79,11 +97,6 @@ let parse = (buffer) => {
     console.log(`im here: ${command}`);
     return { command, payload };
   }
-  //   if (text.startsWith(`${COMMAND_SYMBOL}list`)) {
-  //     let [command, payload] = text.split(/\s+(.*)/);
-  //     console.log('this is the list');
-  //     // return ;
-  //   }
 };
 
 
